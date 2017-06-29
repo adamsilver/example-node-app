60a1495122aaa3f875a332c6036c3d89a5256409
adamsilver <adamsilver@ucds.email>
2017-06-29 15:24:54 +0100

    Form validation

--- src/app/routes.js
+++ src/app/routes.js
@@ -5,6 +5,11 @@ module.exports = function( express, app ){
 		res.render( 'index.html' );
 	} );
 
+	app.get( '/form-validation', function( req, res ){
+
+		res.render( 'form-validation.html' );
+	} );
+
 	app.get( '/colours', function( req, res ){
 
 		res.render( 'colours.html' );
