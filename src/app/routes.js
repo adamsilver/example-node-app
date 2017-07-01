6270d302ba865e4edd21bd9fe7229d87d4cad799
Adam Silver <adambsilver@gmail.com>
2017-07-01 18:41:59 +0100

    better org

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,22 +1,22 @@
 module.exports = function( express, app ){
 
-	app.get( '/', function( req, res ){
-
-		res.render( 'index.html' );
-	} );
-
-	app.get( '/components', function( req, res ){
-
-		res.render( 'components/index.html' );
-	} );
-
-	app.get( '/components/forms/validation', function( req, res ){
-
-		res.render( 'components/forms/validation.html' );
-	} );
+	app.get('/', function( req, res ){
+		res.render('index.html');
+	});
 
 	app.get( '/colours', function( req, res ){
+		res.render('colours.html');
+	});
 
-		res.render( 'colours.html' );
-	} );
+	app.get('/components', function(req, res ){
+		res.render('components/index.html');
+	});
+
+	app.get('/components/validation', function( req, res ){
+		res.render('components/validation.html');
+	});
+
+	app.get('/components/text-box', function( req, res ){
+		res.render('components/text-box.html');
+	});
 };
