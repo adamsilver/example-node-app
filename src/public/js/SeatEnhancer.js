4d47a0890e8bdd8a71460922e597250d1ae3421d
Adam Silver <adambsilver@gmail.com>
2017-07-23 06:58:15 +0100

    Updated

--- src/public/js/SeatEnhancer.js
+++ src/public/js/SeatEnhancer.js
@@ -1,8 +1,9 @@
 function SeatEnhancer() {
-	var checkboxes = $('.plane-seat input');
-	checkboxes.on('focus', $.proxy(this, 'onCheckboxFocus'));
-	checkboxes.on('blur', $.proxy(this, 'onCheckboxBlur'));
-	checkboxes.on('click', $.proxy(this, 'onCheckboxClick'));
+	this.max = 2;
+	this.checkboxes = $('.plane-seat input');
+	this.checkboxes.on('focus', $.proxy(this, 'onCheckboxFocus'));
+	this.checkboxes.on('blur', $.proxy(this, 'onCheckboxBlur'));
+	this.checkboxes.on('click', $.proxy(this, 'onCheckboxClick'));
 }
 
 SeatEnhancer.prototype.onCheckboxFocus = function(e) {
@@ -14,10 +15,54 @@ SeatEnhancer.prototype.onCheckboxBlur = function(e) {
 };
 
 SeatEnhancer.prototype.onCheckboxClick = function(e) {
+	if(this.getCheckedSeats().length > this.max) {
+		var last = this.getLastCheckedSeat();
+		this.markAsUnchecked(last);
+		last[0].checked = false;
+	}
+
 	var checkbox = $(e.target);
 	if(checkbox[0].checked) {
-		checkbox.parents('.plane-seat').addClass('plane-seat-isSelected');
+		this.markAsChecked(checkbox);
+		this.lastChecked = checkbox;
 	} else {
-		checkbox.parents('.plane-seat').removeClass('plane-seat-isSelected');
+		this.markAsUnchecked(checkbox);
+		if(this.lastChecked) {
+			if(checkbox[0] == this.lastChecked[0]) {
+				this.lastChecked = null;
+			}
+		}
+	}
+
+};
+
+SeatEnhancer.prototype.markAsChecked = function(checkbox) {
+	checkbox.parents('.plane-seat').addClass('plane-seat-isSelected');
+};
+
+SeatEnhancer.prototype.markAsUnchecked = function(checkbox) {
+	checkbox.parents('.plane-seat').removeClass('plane-seat-isSelected');
+};
+
+SeatEnhancer.prototype.getLastCheckedSeat = function() {
+	if(this.lastChecked) {
+		return this.lastChecked;
+	} else {
+		var checked = this.getCheckedSeats();
+		if(checked.length) {
+			return $(checked[checked.length-1]);
+		} else {
+			return null;
+		}
 	}
 };
+
+SeatEnhancer.prototype.getCheckedSeats = function() {
+	return this.checkboxes.filter(':checked');
+};
+
+
+
+
+
+
