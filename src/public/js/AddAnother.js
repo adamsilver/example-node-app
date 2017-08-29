01db523ddbb7dc64bb36fff224a8c623a9031679
adamsilver <adamsilver@ucds.email>
2017-08-29 15:29:22 +0100

    Updated

--- src/public/js/AddAnother.js
+++ src/public/js/AddAnother.js
@@ -1,16 +1,15 @@
 function AddAnother(container) {
 	this.container = $(container);
-	this.button = this.container.find('button');
-	this.button.on('click', $.proxy(this, 'buttonOnClick'));
 	this.container.on('click', '.addAnother-removeButton', $.proxy(this, 'onRemoveButtonClick'));
 	this.addRemoveButtons();
+	this.createAddButton();
 }
 
 AddAnother.prototype.addRemoveButtons = function(e) {
 	var items = this.container.find('.addAnother-item');
 	items.each(function(i, el) {
 		if(i != 0) {
-			$(el).append('<button class="addAnother-removeButton" type="button">Remove</button>');
+			$(el).append('<button class="addAnother-removeButton" type="button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 32 32"> <title>plus</title> <path fill="none" d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path> </svg> Remove</button>');
 		}
 	});
 };
@@ -22,7 +21,7 @@ AddAnother.prototype.buttonOnClick = function(e) {
 	var index = items.length;
 	var name = newItem.find('input')[0].name;
 	if(items.length == 1) {
-		newItem.append('<button class="addAnother-removeButton" type="button">Remove</button>');
+		newItem.append('<button class="addAnother-removeButton" type="button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 32 32"> <title>plus</title> <path fill="none" d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path> </svg> Remove </button>');
 	}
 
 	newItem.find('label')[0].htmlFor = name+(index+1);
@@ -38,4 +37,11 @@ AddAnother.prototype.onRemoveButtonClick = function(e) {
 	if(items.length == 1) {
 		items.find('.addAnother-removeButton').remove();
 	}
+};
+
+AddAnother.prototype.createAddButton = function() {
+	this.addButton = $('<button type="button" class="addAnother-addButton">Add another</button>');
+	this.container.append(this.addButton);
+
+	this.addButton.on('click', $.proxy(this, 'buttonOnClick'));
 };
