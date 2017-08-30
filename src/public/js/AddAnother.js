16e2b91772076b75c3d589c34520fefe85b04ffe
adamsilver <adamsilver@ucds.email>
2017-08-30 13:36:42 +0100

    Add another

--- src/public/js/AddAnother.js
+++ src/public/js/AddAnother.js
@@ -9,39 +9,67 @@ AddAnother.prototype.addRemoveButtons = function(e) {
 	var items = this.container.find('.addAnother-item');
 	items.each(function(i, el) {
 		if(i != 0) {
-			$(el).append('<button class="addAnother-removeButton" type="button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 32 32"> <title>plus</title> <path fill="none" d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path> </svg> Remove</button>');
+			$(el).append('<button class="addAnother-removeButton" type="button">- Remove</button>');
 		}
 	});
 };
 
-AddAnother.prototype.buttonOnClick = function(e) {
-	var items = this.container.find('.addAnother-item');
-	var lastItem = items.last();
-	var newItem = lastItem.clone();
-	var index = items.length;
-	var name = newItem.find('input')[0].name;
-	if(items.length == 1) {
-		newItem.append('<button class="addAnother-removeButton" type="button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 32 32"> <title>plus</title> <path fill="none" d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path> </svg> Remove </button>');
-	}
+AddAnother.prototype.onAddButtonClick = function(e) {
+	this.cloneItem();
+	this.updateItems();
+	this.focusLastItem();
+};
 
-	newItem.find('label')[0].htmlFor = name+(index+1);
-	newItem.find('input')[0].id = name+(index+1);
+AddAnother.prototype.cloneItem = function() {
+	var items = this.container.find('.addAnother-item');
+	var item = this.container.find('.addAnother-item')[0];
+	var newItem = $(item).clone();
+	this.appendRemoveButton(newItem);
+	if(items.length === 1) {
+		this.appendRemoveButton($(item));
+	}
 	this.container.find('.addAnother-items').append(newItem);
+};
+
+AddAnother.prototype.appendRemoveButton = function(item) {
+	item.append('<button class="addAnother-removeButton" type="button">- Remove</button>');
+};
+
+AddAnother.prototype.updateItems = function() {
+	this.container.find('.addAnother-item').each(function(index, el) {
+		$(el).find('[data-name], [data-id]').each(function(index2, el) {
+			// var idAttr = $(el).attr('data-name');
+			$(el)[0].name = $(el).attr('data-name').replace(/%index%/, index);
+			$(el)[0].id = $(el).attr('data-id').replace(/%index%/, index);
+
+			var label = $(el).parents('label');
+			if(label[0]) {
+				label[0].htmlFor = $(el)[0].id;
+			} else {
+				label = $(el).prev('label');
+				label[0].htmlFor = $(el)[0].id;
+			}
+		});
+	});
+};
+
+AddAnother.prototype.focusLastItem = function() {
+	this.container.find('.addAnother-item:last .field:first').find('input, textarea, select').focus();
 };
 
 AddAnother.prototype.onRemoveButtonClick = function(e) {
 	var items = this.container.find('.addAnother-item');
-	if(items.length > 1) {
-		$(e.currentTarget).parents('.addAnother-item').remove();
-	}
-	if(items.length == 1) {
+	$(e.currentTarget).parents('.addAnother-item').remove();
+	if(items.length == 2) {
 		items.find('.addAnother-removeButton').remove();
 	}
+	this.updateItems();
+	this.focusLastItem();
 };
 
 AddAnother.prototype.createAddButton = function() {
-	this.addButton = $('<button type="button" class="addAnother-addButton">Add another</button>');
+	this.addButton = $('<button type="button" class="addAnother-addButton">+ Add another</button>');
 	this.container.append(this.addButton);
 
-	this.addButton.on('click', $.proxy(this, 'buttonOnClick'));
+	this.addButton.on('click', $.proxy(this, 'onAddButtonClick'));
 };
